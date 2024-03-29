a78099f60821fdabc5d9e9f84bc89e629fc413b0
mybot4st <164684936+mybot4st@users.noreply.github.com>
2024-03-30 03:32:42 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -1,9 +1,9 @@
 import math
 from config import SUPPORT_CHAT, OWNER_USERNAME
 from pyrogram.types import InlineKeyboardButton
-from DAXXMUSIC import app
+from SHUKLAMUSIC import app
 import config
-from DAXXMUSIC.utils.formatters import time_to_seconds
+from SHUKLAMUSIC.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
